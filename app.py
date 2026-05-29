d8d33a531540616cea1f1f8ead429efd1bc1ad5f
bharath123-R <nagaveni.bhharath1981@gmail.com>
2026-05-29 15:07:19 +0530

    mail issue solved

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from flask import Flask, render_template, request, redirect, url_for
 from models import contact_add
 from flask_mail import Mail, Message
 import os
+import resend
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 mail = Mail(app)
+resend.api_key = "re_RDyB76gw_PWKSr5SAFXnugud9rVho3xZv"
 
 @app.route('/')
 def index():
@@ -44,7 +47,18 @@ Message:
 {message}
 """
 
-            mail.send(msg)
+            resend.Emails.send({
+                "from": "onboarding@resend.dev",
+                "to": ["nagaveni.bharath1981@gmail.com"],
+                "subject": "New Portfolio Contact",
+                "html": f"""
+                <h3>New Contact Message</h3>
+
+                <p><b>Name:</b> {name}</p>
+                <p><b>Email:</b> {email}</p>
+                <p><b>Message:</b> {message}</p>
+                """
+            })
 
             return redirect(url_for('index'))
 
